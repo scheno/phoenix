8bcb3b3144da245a89e6d10b5951792731840f93
chenshen <chenshen@zjft.com>
2020-07-06 16:05:28 +0800

    Spring Security OAuth2 JWT

--- spring-security/distributed-security-uaa/src/main/java/com/schening/phoenix/security/uaa/config/AuthorizationServerConfig.java
+++ spring-security/distributed-security-uaa/src/main/java/com/schening/phoenix/security/uaa/config/AuthorizationServerConfig.java
@@ -1,11 +1,9 @@
 package com.schening.phoenix.security.uaa.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.token.TokenService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
